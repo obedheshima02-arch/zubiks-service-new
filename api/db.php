d9d34150e9dbe3006aaddb539fdedbef24858728
Obed <obedheshima02@gmail.com>
2026-09-08 22:59:00 +0200

    Fix REQUEST_METHOD CLI notice in db.php

--- api/db.php
+++ api/db.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+if (($_SERVER['REQUEST_METHOD'] ?? '') === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
